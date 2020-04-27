f15d097f29a753cb2f606bc460f0048f241a64d7
Jehan <jehan@girinstud.io>
2020-04-27 15:24:17 +0200

    src: fix the usage of `uchardet` tool.
    It was displaying -v for both verbose and version options. The new
    --verbose short option is actually -V (uppercase).

--- src/tools/uchardet.cpp
+++ src/tools/uchardet.cpp
@@ -115,7 +115,7 @@ void show_usage()
     printf("Options:\n");
     printf(" -v, --version         Print version and build information.\n");
     printf(" -h, --help            Print this help.\n");
-    printf(" -v, --verbose         Show all candidates and their confidence value.\n");
+    printf(" -V, --verbose         Show all candidates and their confidence value.\n");
     printf("\n");
 }
 
